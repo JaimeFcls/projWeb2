484ba7f3e3bf2f6d6d8c43941d8b8812eac71a75
Jaime <jfcls@discente.ifpe.edu.br>
2023-06-10 18:05:49 -0300

    att

--- back.js
+++ back.js
@@ -6,11 +6,10 @@ app.use(express.urlencoded({ extended: false }));
 
 
 app.post('/pedidos', (req, res) => {
-    const { numPedido, itemPedido, valorPedido, horaPedido, minutosPedido, anoPedido } = req.body;
-
+    var { numPedido, itemPedido, valorPedido, horaPedido, minutosPedido, anoPedido } = req.body; 
     res.send(`N° do Pedido: ${numPedido}, Produto: ${itemPedido}, valor: R$ ${valorPedido}, Comprado às: ${horaPedido}:${minutosPedido} de ${anoPedido}`);
 });
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
-});
+}); 
