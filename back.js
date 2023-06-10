8ad908d550133d5407fae68204a16f89af1fe103
Jaime <jfcls@discente.ifpe.edu.br>
2023-06-09 22:20:35 -0300

    att2

--- back.js
+++ back.js
@@ -1,20 +1,16 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
-//
-app.use(bodyParser.urlencoded({ extended: false }));
-//
-app.post('/pedidos', (req, res) => {
-    var numPedido = req.body.numPedido;
-    var itemPedido = req.body.itemPedido;
-    var valorPedido = req.body.valorPedido;
-    var horaPedido = req.body.horaPedido;
-    var minutosPedido = req.body.minutosPedido;
-    var anoPedido = req.body.anoPedido;
+const port = 3000;
 
-    res.send('N° do Pedido : ' + numPedido + ' , Produto : ' + itemPedido + ' , valor : R$ ' + valorPedido + ' Comprado às : ' + horaPedido + ' e ' + minutosPedido + ' de ' + anoPedido);
+app.use(express.urlencoded({ extended: false }));
+
+
+app.post('/pedidos', (req, res) => {
+    const { numPedido, itemPedido, valorPedido, horaPedido, minutosPedido, anoPedido } = req.body;
+
+    res.send(`N° do Pedido: ${numPedido}, Produto: ${itemPedido}, valor: R$ ${valorPedido}, Comprado às: ${horaPedido}:${minutosPedido} de ${anoPedido}`);
 });
-//
-app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000');
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
 });
