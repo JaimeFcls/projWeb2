9bd1478fb32246e5b68e0c860dc34c32710567b0
Jaime <jfcls@discente.ifpe.edu.br>
2023-06-09 22:14:40 -0300

    att

--- back.js
+++ back.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
+//
 app.use(bodyParser.urlencoded({ extended: false }));
-
+//
 app.post('/pedidos', (req, res) => {
     var numPedido = req.body.numPedido;
     var itemPedido = req.body.itemPedido;
@@ -14,7 +14,7 @@ app.post('/pedidos', (req, res) => {
 
     res.send('N° do Pedido : ' + numPedido + ' , Produto : ' + itemPedido + ' , valor : R$ ' + valorPedido + ' Comprado às : ' + horaPedido + ' e ' + minutosPedido + ' de ' + anoPedido);
 });
-
+//
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
 });
---
+++ readMe.txt
@@ -0,0 +1,3 @@
+DUPLA F - JAIME e LETICIA
+
+Testado via PostMan no método POST no formato x-www-form-urlencoded , recebendo dados referente á um pedido gerado.
