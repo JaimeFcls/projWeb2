8b0c00fc5745c21aa14c441e68b662cffd7911bf
Jaime <jfcls@discente.ifpe.edu.br>
2023-06-09 22:05:50 -0300

    newCommit

--- back.js
+++ back.js
@@ -2,15 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/pedidos', (req, res) => {
-    const pedidos = req.body;
-    console.log(pedidos);
-    console.log(pedidos);
+    var numPedido = req.body.numPedido;
+    var itemPedido = req.body.itemPedido;
+    var valorPedido = req.body.valorPedido;
+    var horaPedido = req.body.horaPedido;
+    var minutosPedido = req.body.minutosPedido;
+    var anoPedido = req.body.anoPedido;
 
-    // Enviar os pedidos como resposta para o navegador
-    res.send(JSON.stringify(pedidos));
+    res.send('N° do Pedido : ' + numPedido + ' , Produto : ' + itemPedido + ' , valor : R$ ' + valorPedido + ' Comprado às : ' + horaPedido + ' e ' + minutosPedido + ' de ' + anoPedido);
 });
 
 app.listen(3000, () => {
